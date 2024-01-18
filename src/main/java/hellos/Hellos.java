b762d612cba152627b998e61dfee9fbd7bf0828f
alijawosti <alijawosti@gmail.com>
2024-01-18 14:17:22 -0600

    Added the greetings!

--- src/main/java/hellos/Hellos.java
+++ src/main/java/hellos/Hellos.java
@@ -8,19 +8,28 @@ public class Hellos {
 
   public String generateOutput() {
     StringBuilder builder = new StringBuilder();
-
+    builder.append(alijaSaysHello());
     builder.append(kkSaysHello());
+    builder.append(linneaSaysHello());
     builder.append(nicSaysHello());
 
     return builder.toString();
   }
 
-  private String nicSaysHello() {
-    return "Nic says 'Howdy!'\n";
+  private String alijaSaysHello() {
+    return "Alija says 'Howdy!'\n";
   }
 
   private String kkSaysHello() {
     return "KK says 'Hello!'\n";
+  }
+
+  private String linneaSaysHello() {
+    return "Linnea says 'Howdy!'\n";
+  }
+
+  private String nicSaysHello() {
+    return "Nic says 'Howdy!'\n";
   }
 
 }
