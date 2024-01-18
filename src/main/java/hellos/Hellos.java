75135741fb05cd37ab104b130359bb65fbb38914
Jack <mahon480@morris.umn.edu>
2024-01-18 14:09:21 -0600

    Add greeting from Jack.

--- src/main/java/hellos/Hellos.java
+++ src/main/java/hellos/Hellos.java
@@ -9,10 +9,16 @@ public class Hellos {
   public String generateOutput() {
     StringBuilder builder = new StringBuilder();
 
+    builder.append(jackSaysHello());
     builder.append(kkSaysHello());
     builder.append(nicSaysHello());
 
     return builder.toString();
+  }
+
+  private String jackSaysHello() {
+    // TODO Auto-generated method stub
+    return "Jack says 'Hello!'\n";
   }
 
   private String nicSaysHello() {
